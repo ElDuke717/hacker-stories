0424586b0d29cd0850f7b5d6c8d67da8216cda8c
Nick Huemmer <njhuemmer@gmail.com>
2021-09-24 12:14:24 -0400

    added event handler for input field

--- src/App.js
+++ src/App.js
@@ -39,44 +39,44 @@ const list = [
   }
 ];
 
-const  App = () =>    (
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
+
+    <Search />
+
+    <hr />
+
+    <List />
+  </div>
+);
+
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
     <div>
-      <h1>My Hacker Stories</h1>
-
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-
-      <hr />
-
-      <List />
-      <List />
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
-
-
-const Search = () =>   (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-      </div>
-  );
-
+};
 
 const List = () => (
   <ul>
-        {list.map( (item) => {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
-  );
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+);
 
 export default App;
