8a4c7e269c2f9652bc7fbccf0e4295a4545c1c5c
Nick Huemmer <njhuemmer@gmail.com>
2021-09-26 10:11:27 -0400

    Lifting State in React Complete

--- src/App.js
+++ src/App.js
@@ -35,9 +35,16 @@ const App = () => {
       objectID: 3,
     }
   ];
-  const handleSearch = (event) =>{
-    console.log(event.target.value);
-  }
+  
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter( (story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 return (
   <div>
     <h1>My Hacker Stories</h1>
@@ -46,30 +53,18 @@ return (
 
     <hr />
 
-    <List list={stories}/>
+    <List list={searchedStories}/>
   </div>
 );
 }
 
-const Search = (props) => {
-  const [searchTerm, setSearchTerm] = React.useState('');
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-    props.onSearch(event);
-  };
-
-  return (
+const Search = (props) =>  (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-      </p>
+      <input id="search" type="text" onChange={props.onSearch} />
     </div>
   );
-};
+
 
 
 const List = (props) => (
